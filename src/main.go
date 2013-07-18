2d8f4e239fe61b810145a972057f35881f05fb6a
Florin Patan <florinpatan@gmail.com>
2013-07-18 15:58:06 +0200

    Added directory parsing options. Can aggregate data or display them for each file

--- src/main.go
+++ src/main.go
@@ -17,11 +17,13 @@ import (
     "flag"
     "log"
     "sort"
+    "io/ioutil"
+    "regexp"
 )
 
-var fileName, urlPrefix, inputFileFormat string;
+var fileName, urlPrefix, inputFileFormat, fileRegEx, directoryName string;
 var maxUrls int;
-var showHits, showStatistics, fullDisplay bool;
+var showHits, showStatistics, fullDisplay, aggregateData bool;
 
 type Key string
 type HitCount int
@@ -70,71 +72,29 @@ func parseLine (line string) string {
 func init() {
     flag.StringVar(&inputFileFormat, "fmt", "nginx", "Set the input file format. Can be: nginx, apache, cloudfront. Default: nginx");
 
-    flag.StringVar(&fileName, "f", "", "Set the name of the file to be parsed, default empty");
+    flag.StringVar(&fileName, "f", "", "Name of the file to be parsed, default empty.");
 
-    flag.IntVar(&maxUrls, "l", 0, "Set the number of lines to be parsed, default, all, 0 = all");
+    flag.StringVar(&fileRegEx, "fr", ".*", "Regex of the files to be parsed, default (.*) all. If used it will override -f. Must be used with -dir option.");
 
-    flag.BoolVar(&showHits, "h", false, "Show the hits for the urls, default false");
+    flag.StringVar(&directoryName, "dir", "", "Directory name of where the files should be loaded from, default empty. If used it will override -f. Must be used with -fr option.");
 
-    flag.StringVar(&urlPrefix, "p", "", "Set the prefix for the urls to be displayed, default empty");
+    flag.IntVar(&maxUrls, "l", 0, "Number of lines to be parsed, default, all, 0 = all.");
 
-    flag.BoolVar(&showStatistics, "s", false, "Show statistics for hits of the urls, default false");
+    flag.BoolVar(&showHits, "h", false, "Show the hits for the urls, default false.");
 
-    flag.BoolVar(&fullDisplay, "fd", false, "Just extract the whole urls and do nothing else to process them. Overrides all other switches but limit and prefix");
+    flag.StringVar(&urlPrefix, "p", "", "Set the prefix for the urls to be displayed, default empty.");
+
+    flag.BoolVar(&showStatistics, "s", false, "Show statistics for hits of the urls, default false.");
+
+    flag.BoolVar(&fullDisplay, "fd", false, "Just extract the whole urls and do nothing else to process them. Overrides all other switches but limit and prefix.");
+
+    flag.BoolVar(&aggregateData, "a", false, "Aggregate data from all input files. Must be used with -dir option.");
 }
 
-func main() {
-
-    flag.Parse();
-
-    if fileName == "" {
-        log.Fatalln("Filename not specified");
-    }
-
-    if (showStatistics) {
-        showHits = true;
-    }
-
-    urlHits := make(map [Key]HitCount);
-
-    f, err := os.Open(fileName);
-    if err != nil {
-        fmt.Printf("Error opening file: %v\n",err);
-        os.Exit(1);
-    }
-    defer f.Close();
-    r := bufio.NewReader(f);
+func displayOutput(urlHits map[Key]HitCount, urlCount int) {
 
     largestHit := 0;
     largestHitURL := "";
-
-    if (inputFileFormat == "cloudfront") {
-        r.ReadLine();
-        r.ReadLine();
-    }
-
-    s, _, e := r.ReadLine();
-    urlCount := 0;
-    for e == nil {
-        url := parseLine(string(s));
-        if (fullDisplay) {
-            fmt.Printf("%s%s\n", urlPrefix, url);
-        } else {
-            urlHits[Key(url)] += 1;
-        }
-
-        s, _, e = r.ReadLine();
-
-        urlCount++;
-        if (maxUrls != 0 && urlCount > maxUrls) {
-            break;
-        }
-    }
-    f.Close();
-
-    if (fullDisplay) {
-        return;
-    }
 
     if (showHits) {
         if (showStatistics) {
@@ -171,4 +131,95 @@ func main() {
             fmt.Printf("%s%s\n", urlPrefix, key);
         }
     }
+}
+
+func main() {
+
+    flag.Parse();
+
+    if (fileName == "" && directoryName == "" && fileRegEx == "")  {
+        log.Fatalln("Filename or directory not specified");
+    }
+
+    if (showStatistics) {
+        showHits = true;
+    }
+
+    var fileList []string;
+
+    if (fileName == "") {
+        includeRegex, err := regexp.Compile(fileRegEx);
+        if err != nil {
+            log.Fatal(err);
+        }
+
+        files, _ := ioutil.ReadDir(directoryName);
+
+        for _, f := range files {
+            if (!f.IsDir() && includeRegex.Match([]byte(f.Name()))) {
+                fileList = append(fileList, directoryName + f.Name());
+            }
+        }
+
+    } else {
+        fileList[0] = fileName;
+    }
+
+    urlCount := 0;
+    urlHits := make(map [Key]HitCount);
+
+    for _, fileName := range fileList {
+
+        f, err := os.Open(fileName);
+        if err != nil {
+            fmt.Printf("Error opening file: %v\n",err);
+            os.Exit(1);
+        }
+
+        defer f.Close();
+
+        r := bufio.NewReader(f);
+
+        if (inputFileFormat == "cloudfront") {
+            r.ReadLine();
+            r.ReadLine();
+        }
+
+        if (!aggregateData) {
+            fmt.Printf("\n\nAnalyzing file: %s\n", fileName);
+        }
+
+        s, _, e := r.ReadLine();
+        for e == nil {
+            url := parseLine(string(s));
+            if (fullDisplay) {
+                fmt.Printf("%s%s\n", urlPrefix, url);
+            } else {
+                urlHits[Key(url)] += 1;
+            }
+
+            s, _, e = r.ReadLine();
+
+            urlCount++;
+            if (maxUrls != 0 && urlCount > maxUrls) {
+                break;
+            }
+        }
+        f.Close();
+
+        if (fullDisplay) {
+            return;
+        }
+
+        if (!aggregateData) {
+            displayOutput(urlHits, urlCount);
+            urlCount = 0;
+            urlHits = make(map [Key]HitCount);
+        }
+    }
+
+    if (aggregateData) {
+        displayOutput(urlHits, urlCount);
+    }
+
 }
